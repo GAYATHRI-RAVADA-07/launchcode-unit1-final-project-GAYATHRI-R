25fdbb28406a4570b64cc52d15f23e23490695cd
Gayathri Ravada <gayathri.ravada7@gmail.com>
2026-08-10 13:56:05 -0500

    Add match search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,29 @@ import { matches as initialMatches } from "./mockdata/matches";
 
 function App() {
   const [matches, setMatches] = useState(initialMatches);
+  const [searchTerm, setSearchTerm] = useState("");
 
   function handleCreateMatchForm(newMatch) {
     setMatches((currentMatches) => [...currentMatches, newMatch]);
   }
+
+  const filteredMatches = matches.filter((game) =>
+    game.sport.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
   return (
     <div>
       <h1>CourtConnect</h1>
 
       <CreateMatchForm onCreateMatch={handleCreateMatchForm} />
 
-      <MatchList games={matches} />
+      <input
+        type="text"
+        placeholder="Search by sport..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <MatchList games={filteredMatches} />
     </div>
   );
 }
