0f16f435c191db338d6746b1dd5dcf62e4c1fd09
Gayathri Ravada <gayathri.ravada7@gmail.com>
2026-08-20 18:25:39 -0500

    Added mockdata for user

---
+++ src/mockdata/user.js
@@ -0,0 +1,9 @@
+export const user = {
+  name: "Gayathri Ravada",
+  skillLevel: "Intermediate",
+  email: "gayathri@example.com",
+  phone: "(314) 555-0123",
+  preferredSports: ["Tennis", "Pickleball"],
+  aboutMe:
+    "I enjoy playing tennis and pickleball and meeting other players in the local community.",
+};
